9f03eff98c4de77702788473bcc951d34d4e618d
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2025-05-17 09:24:49 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -6,7 +6,7 @@ import numpy as np
 from sqlalchemy import create_engine, inspect
 from sqlalchemy import text
 
-warehouse = "postgresql://duckdb_sample_user:i6iKJc6FCs4hVS3AX6yMZngxJvMkzGCs@dpg-d0b2efp5pdvs73c9pi00-a.singapore-postgres.render.com/duckdb_sample"
+warehouse = "postgresql://duckdb_sample_user:i6iKJc6FCs4hVS3AX6yMZngxJvMkzGCs@dpg-d0b2efp5pdvs73c9pi00-a/duckdb_sample"
 engine = create_engine(warehouse,  client_encoding='utf8')
 connection = engine.connect()
 
@@ -14,7 +14,7 @@ connection = engine.connect()
 def load_data():
     query_ext = """
         SELECT "Product", count(*) AS count
-        FROM all_data
+        FROM sales_data_duckdb
         GROUP BY "Product";
     """
     result = connection.execute(text(query_ext))
@@ -22,9 +22,6 @@ def load_data():
 
 df = load_data()
 
-
-
 st.title("Sales Dashboard")
 st.subheader("Most bought product")
 st.bar_chart(df.set_index('Product'))
-
